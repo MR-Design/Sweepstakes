29bb8046e3c7cb4789e74cc19e85dff7ff78a94f
MR-Design <two7city@gmail.comMR-Design>
2018-10-10 07:36:19 -0500

    Made Classes & Interfaces Needed For the Project

---
+++ Sweepstakes/Sweepstakes/Contestant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        string firstName;
+        string  lastName ;
+        string email;
+        string registrationNumber;
+
+    }
+}
